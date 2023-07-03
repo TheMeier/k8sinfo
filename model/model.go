f9f47d1178cc30fe7ce8470dde20a89f61f4ca94
Christoph Maser <christoph.maser@1und1.de>
2023-07-03 16:56:00 +0200

    fix: update ingress to v1

--- model/model.go
+++ model/model.go
@@ -6,20 +6,20 @@ import (
 	"github.com/globalsign/mgo/bson"
 	apps "k8s.io/api/apps/v1"
 	core "k8s.io/api/core/v1"
-	v1beta1 "k8s.io/api/extensions/v1beta1"
+	networking "k8s.io/api/networking/v1"
 )
 
 // K8sInfoElement holds info data for one context
 type K8sInfoElement struct {
 	Deployments *apps.DeploymentList
 	Services    *core.ServiceList
-	Ingresses   *v1beta1.IngressList
+	Ingresses   *networking.IngressList
 }
 
 // K8sInfoData holds all scraped data for all contexts
 type K8sInfoData map[string]*K8sInfoElement
 
-//DeploymentElement wraps a deployment for storage in a backend
+// DeploymentElement wraps a deployment for storage in a backend
 type DeploymentElement struct {
 	ID         bson.ObjectId    `bson:"_id,omitempty"`
 	Deployment *apps.Deployment `bson:",inline"`
@@ -27,7 +27,7 @@ type DeploymentElement struct {
 	Timestamp  time.Time        `bson:"timestamp"`
 }
 
-//ServiceElement wraps a deployment for storage in a backend
+// ServiceElement wraps a deployment for storage in a backend
 type ServiceElement struct {
 	ID        bson.ObjectId `bson:"_id,omitempty"`
 	Service   *core.Service `bson:",inline"`
@@ -35,10 +35,10 @@ type ServiceElement struct {
 	Timestamp time.Time     `bson:"timestamp"`
 }
 
-//IngressElement wraps a deployment for storage in a backend
+// IngressElement wraps a deployment for storage in a backend
 type IngressElement struct {
-	ID        bson.ObjectId    `bson:"_id,omitempty"`
-	Ingress   *v1beta1.Ingress `bson:",inline"`
-	Name      string           `bson:"name"`
-	Timestamp time.Time        `bson:"timestamp"`
+	ID        bson.ObjectId       `bson:"_id,omitempty"`
+	Ingress   *networking.Ingress `bson:",inline"`
+	Name      string              `bson:"name"`
+	Timestamp time.Time           `bson:"timestamp"`
 }
